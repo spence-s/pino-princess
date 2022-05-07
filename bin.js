e4c11a0acf5fdc74f25948a636b3a201a5da52df
Spencer Snyder <sasnyde2@gmail.com>
2022-05-06 22:23:20 -0400

    feat: working config + blacklist/whitelist

--- bin.js
+++ bin.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 
 const process = require('node:process');
+const os = require('node:os');
 const pump = require('pump');
+const {cosmiconfigSync} = require('cosmiconfig');
 const build = require('.');
 
-const res = build();
+const explorer = cosmiconfigSync('pino-prettier', {stopDir: os.homedir()});
+const {config} = explorer.search(process.cwd()) || {};
+
+const res = build(config);
 pump(process.stdin, res);
