3fa7cebead50f8aac3bf36fd0dfb0d778899de03
Spencer Snyder <sasnyde2@gmail.com>
2022-06-26 17:16:50 -0400

    feat: whitelist + blacklist

--- cli.js
+++ cli.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
-
+const fs = require('fs');
 const process = require('node:process');
 const os = require('node:os');
 const pump = require('pump');
 const {cosmiconfigSync} = require('cosmiconfig');
 const build = require('.');
 
-const explorer = cosmiconfigSync('pino-prettier', {stopDir: os.homedir()});
+const explorer = cosmiconfigSync('pino-princess', {stopDir: os.homedir()});
 const {config} = explorer.search(process.cwd()) || {};
 
 const res = build(config);
 pump(process.stdin, res);
+
+if (!process.stdin.isTTY && !fs.fstatSync(process.stdin.fd).isFile()) {
+  process.once('SIGINT', function () {});
+}
